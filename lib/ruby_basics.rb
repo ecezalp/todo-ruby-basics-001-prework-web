02901a3243fc8655565d4ba9cc0db260e48148e1
Ece Ozalp <ozale272@newschool.edu>
2016-04-27 15:46:56 -0400

    defined everthing

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+ x = num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(x)
+  x = "Bob"
 end
 
-def argue
-  
+def argue(argument)
+  puts argument
+  return argument
 end
 
-def greeting
-
+def greeting(x,y)
+  puts "#{x}, #{y}"
 end
 
 def return_a_value
-
+  puts "Nice"
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  puts phrase = "expert"
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(x = "cheese")
+  puts x
+  return x
 end
